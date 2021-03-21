deb31440661c95552043c8140e5a82f4acebddfe
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-03-21 20:48:32 +0200

    first commit

---
+++ answers.sh
@@ -0,0 +1,31 @@
+#Part 1
+
+SELECT Name FROM students
+SELECT * FROM students WHERE Age > 30
+SELECT Name FROM students WHERE Gender = "F" AND Age = 30
+SELECT Points FROM students WHERE Name="Alex"
+INSERT INTO students VALUES (8, "Ibrahim", 25, "M", 350)
+UPDATE students SET Points = Points + 100 WHERE Name = "Basma"
+UPDATE students SET Points = Points - 50 WHERE Name = "Alex"
+
+#Part 2
+
+INSERT INTO graduates (ID, Name, Age, Gender, Points) SELECT * FROM students WHERE Name = "Layal"
+UPDATE graduates SET Graduation="08/09/2018" WHERE Name="Layal"   
+DELETE FROM students WHERE Name = "Layal" 
+
+#Part 3
+
+SELECT employees.Name, employees.Company, companies.Date FROM employees INNER JOIN companies ON employees.Company=companies.Name
+SELECT employees.Name FROM employees INNER JOIN companies ON employees.Company=companies.Name WHERE companies.Date<2000
+SELECT companies.Name FROM employees INNER JOIN companies ON employees.Company=companies.Name WHERE employees.Role='Graphic Designer'
+
+
+
+#Part 4
+
+SELECT Name FROM students WHERE Points=(SELECT max(Points) FROM students)
+SELECT avg(Points) FROM students
+SELECT count() FROM students WHERE Points = 500
+SELECT Name FROM students WHERE Name like '%s%'
+SELECT Name FROM students ORDER BY Points DESC
